eddb6d67b1ccef54e0f3f93390ea6bd425e40f16
Leandro Lisboa Penz <lpenz@lpenz.org>
2023-12-10 09:54:35 +0000

    Day 10a

---
+++ day10/src/bin/day10a.rs
@@ -0,0 +1,53 @@
+// Copyright (C) 2023 Leandro Lisboa Penz <lpenz@lpenz.org>
+// This file is subject to the terms and conditions defined in
+// file 'LICENSE', which is part of this source code package.
+
+use std::io::{stdin, BufRead};
+
+use day10::*;
+
+fn process(bufin: impl BufRead) -> Result<usize> {
+    let input = parser::parse(bufin)?;
+    let mut grid = Grid::default();
+    let mut start = Qa::default();
+    for (y, line) in input.into_iter().enumerate() {
+        for (x, cell) in line.into_iter().enumerate() {
+            let qa = Qa::try_from((x as u16, y as u16))?;
+            grid[qa] = cell;
+            if cell == Cell::Start {
+                start = qa;
+            }
+        }
+    }
+    for qr0 in [Qr::N, Qr::E, Qr::S, Qr::W] {
+        let mut steps = 0;
+        let mut qr = qr0;
+        let mut qa = start;
+        while let Ok(next_qa) = qa + qr {
+            qa = next_qa;
+            steps += 1;
+            if qa == start {
+                return Ok(steps / 2);
+            }
+            if let Some(next_qr) = next_qr(&grid, qa, qr) {
+                qr = next_qr;
+            } else {
+                break;
+            }
+        }
+    }
+    Ok(5)
+}
+
+#[test]
+fn test() -> Result<()> {
+    assert_eq!(process(EXAMPLE1.as_bytes())?, 4);
+    assert_eq!(process(EXAMPLE2.as_bytes())?, 8);
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    color_eyre::install()?;
+    println!("{}", process(stdin().lock())?);
+    Ok(())
+}
---
+++ day10/src/lib.rs
@@ -0,0 +1,102 @@
+// Copyright (C) 2023 Leandro Lisboa Penz <lpenz@lpenz.org>
+// This file is subject to the terms and conditions defined in
+// file 'LICENSE', which is part of this source code package.
+
+pub use color_eyre::{eyre::eyre, Report, Result};
+
+pub const EXAMPLE1: &str = "-L|F7
+7S-7|
+L|7||
+-L-J|
+L|-JF
+";
+
+pub const EXAMPLE2: &str = "7-F7-
+.FJ|7
+SJLL7
+|F--J
+LJ.LJ
+";
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Default)]
+pub enum Cell {
+    #[default]
+    Ground,
+    Start,
+    NS,
+    EW,
+    NE,
+    NW,
+    SW,
+    SE,
+}
+
+pub use sqrid::Qr;
+pub type Sqrid = sqrid::sqrid_create!(140, 140, false);
+pub type Qa = sqrid::qa_create!(Sqrid);
+pub type Grid = sqrid::grid_create!(Sqrid, Cell);
+
+pub fn next_qr(grid: &Grid, qa: Qa, qr: Qr) -> Option<Qr> {
+    match (grid[qa], qr) {
+        (Cell::NS, Qr::N) => Some(Qr::N),
+        (Cell::NS, Qr::S) => Some(Qr::S),
+        (Cell::EW, Qr::E) => Some(Qr::E),
+        (Cell::EW, Qr::W) => Some(Qr::W),
+        (Cell::NE, Qr::W) => Some(Qr::N),
+        (Cell::NE, Qr::S) => Some(Qr::E),
+        (Cell::NW, Qr::E) => Some(Qr::N),
+        (Cell::NW, Qr::S) => Some(Qr::W),
+        (Cell::SW, Qr::E) => Some(Qr::S),
+        (Cell::SW, Qr::N) => Some(Qr::W),
+        (Cell::SE, Qr::W) => Some(Qr::S),
+        (Cell::SE, Qr::N) => Some(Qr::E),
+        _ => None,
+    }
+}
+
+impl TryFrom<char> for Cell {
+    type Error = Report;
+    fn try_from(c: char) -> Result<Self, Self::Error> {
+        match c {
+            '|' => Ok(Cell::NS),
+            '-' => Ok(Cell::EW),
+            'L' => Ok(Cell::NE),
+            'J' => Ok(Cell::NW),
+            '7' => Ok(Cell::SW),
+            'F' => Ok(Cell::SE),
+            '.' => Ok(Cell::Ground),
+            'S' => Ok(Cell::Start),
+            other => Err(eyre!("invalid pipe {}", other)),
+        }
+    }
+}
+
+pub mod parser {
+    use aoc::parser::*;
+
+    use super::*;
+
+    fn cell(input: &str) -> IResult<&str, Cell> {
+        let (input, c) = character::one_of("|-LJ7F.S")(input)?;
+        Ok((input, Cell::try_from(c).unwrap()))
+    }
+
+    fn line(input: &str) -> IResult<&str, Vec<Cell>> {
+        let (input, cells) = multi::many1(cell)(input)?;
+        let (input, _) = character::newline(input)?;
+        Ok((input, cells))
+    }
+
+    pub fn parse(mut bufin: impl BufRead) -> Result<Vec<Vec<Cell>>> {
+        aoc::parse_with!(multi::many1(line), bufin)
+    }
+}
+
+#[test]
+fn test() -> Result<()> {
+    assert_eq!(parser::parse(EXAMPLE1.as_bytes())?.len(), 5);
+    assert_eq!(parser::parse(EXAMPLE1.as_bytes())?[0].len(), 5);
+    assert_eq!(parser::parse(EXAMPLE2.as_bytes())?.len(), 5);
+    assert_eq!(parser::parse(EXAMPLE2.as_bytes())?[0].len(), 5);
+    Ok(())
+}
