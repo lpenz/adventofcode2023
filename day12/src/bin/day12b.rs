a47537d524b3a4e139787df9638b184a5cbff511
Leandro Lisboa Penz <lpenz@lpenz.org>
2023-12-12 21:26:01 +0000

    Day 12b

--- day12/src/bin/day12b.rs
+++ day12/src/bin/day12b.rs
@@ -6,17 +6,28 @@ use std::io::{stdin, BufRead};
 
 use day12::*;
 
+pub fn unfold_calc_arrangements(row: &Row, record: &[u32]) -> usize {
+    let mut bigrow = row.clone();
+    let mut bigrecord = record.to_vec();
+    for _ in 0..4 {
+        bigrow.0.push(Cell::Unknown);
+        bigrow.0.extend(row.0.iter());
+        bigrecord.extend(record.iter());
+    }
+    calc_arrangements(&bigrow, &bigrecord)
+}
+
 fn process(bufin: impl BufRead) -> Result<usize> {
     let input = parser::parse(bufin)?;
     Ok(input
         .into_iter()
-        .map(|entry| calc_arrangements(&entry.0, &entry.1))
+        .map(|entry| unfold_calc_arrangements(&entry.0, &entry.1))
         .sum())
 }
 
 #[test]
 fn test() -> Result<()> {
-    assert_eq!(process(EXAMPLE.as_bytes())?, 21);
+    assert_eq!(process(EXAMPLE.as_bytes())?, 525152);
     Ok(())
 }
 
--- day12/src/lib.rs
+++ day12/src/lib.rs
@@ -43,8 +43,14 @@ impl TryFrom<char> for Cell {
     }
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Hash, PartialEq, Eq)]
 pub struct Row(pub Vec<Cell>);
+
+impl Row {
+    pub fn subrow(&self, from: usize) -> Row {
+        Row(self.0[from..].to_vec())
+    }
+}
 
 impl fmt::Display for Row {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
@@ -91,5 +97,118 @@ pub mod parser {
 #[test]
 fn test() -> Result<()> {
     assert_eq!(parser::parse(EXAMPLE.as_bytes())?.len(), 6);
+    Ok(())
+}
+
+pub type Cache = std::collections::HashMap<(Row, Vec<u32>), usize>;
+
+pub fn helper(
+    cache: &mut Cache,
+    row: &mut Row,
+    irow: usize,
+    record: Vec<u32>,
+    currun: usize,
+    _lastok: bool,
+) -> usize {
+    if irow == row.0.len() {
+        let success = record.is_empty() || record == vec![0];
+        return if success { 1 } else { 0 };
+    }
+    let key = (row.subrow(irow), record.clone());
+    if let Some(result) = cache.get(&key) {
+        return *result;
+    }
+    match row.0[irow] {
+        Cell::Ok => {
+            let mut newrecord = record.clone();
+            if currun > 0 {
+                // End run
+                if newrecord[0] != 0 {
+                    return 0;
+                }
+                newrecord.remove(0);
+            }
+            helper(cache, row, irow + 1, newrecord, 0, true)
+        }
+        Cell::Broken => {
+            if record.is_empty() || record[0] == 0 {
+                // We can't have another broken
+                return 0;
+            }
+            let mut newrecord = record.clone();
+            newrecord[0] -= 1;
+            helper(cache, row, irow + 1, newrecord, currun + 1, false)
+        }
+        Cell::Unknown => {
+            row.0[irow] = Cell::Ok;
+            let newrecord1 = record.clone();
+            let oksum = helper(cache, row, irow, newrecord1, currun, _lastok);
+            let brksum = if !record.is_empty() || record != vec![0] {
+                row.0[irow] = Cell::Broken;
+                let newrecord2 = record.clone();
+                helper(cache, row, irow, newrecord2, currun, _lastok)
+            } else {
+                0
+            };
+            row.0[irow] = Cell::Unknown;
+            cache.insert(key, oksum + brksum);
+            oksum + brksum
+        }
+    }
+}
+
+pub fn calc_arrangements(row: &Row, record: &[u32]) -> usize {
+    let mut mrow = row.clone();
+    let rec = record.to_vec();
+    let mut cache = Cache::new();
+    helper(&mut cache, &mut mrow, 0, rec, 0, true)
+}
+
+#[test]
+fn test_calc_arrangements_1() -> Result<()> {
+    let row = "???.###".parse::<Row>()?;
+    let record = vec![1, 1, 3];
+    assert_eq!(calc_arrangements(&row, &record), 1);
+    Ok(())
+}
+
+#[test]
+fn test_calc_arrangements_2() -> Result<()> {
+    let row = ".??..??...?##.".parse::<Row>()?;
+    let record = vec![1, 1, 3];
+    assert_eq!(calc_arrangements(&row, &record), 4);
+    Ok(())
+}
+
+#[test]
+fn test_calc_arrangements_3() -> Result<()> {
+    let row = "?#?#?#?#?#?#?#?".parse::<Row>()?;
+    let record = vec![1, 3, 1, 6];
+    assert_eq!(calc_arrangements(&row, &record), 1);
+    Ok(())
+}
+
+#[test]
+fn test_calc_arrangements_4() -> Result<()> {
+    // let row = "????.#...#...".parse::<Row>()?;
+    let row = "####.#...#...".parse::<Row>()?;
+    let record = vec![4, 1, 1];
+    assert_eq!(calc_arrangements(&row, &record), 1);
+    Ok(())
+}
+
+#[test]
+fn test_calc_arrangements_5() -> Result<()> {
+    let row = "????.######..#####.".parse::<Row>()?;
+    let record = vec![1, 6, 5];
+    assert_eq!(calc_arrangements(&row, &record), 4);
+    Ok(())
+}
+
+#[test]
+fn test_calc_arrangements_6() -> Result<()> {
+    let row = "?###????????".parse::<Row>()?;
+    let record = vec![3, 2, 1];
+    assert_eq!(calc_arrangements(&row, &record), 10);
     Ok(())
 }
